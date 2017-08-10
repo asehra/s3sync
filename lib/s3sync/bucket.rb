ab869916004338a537cdbd0ce3ea05be4eeba14f
Ashish Sehra <ashish.sehra@hsbc.com>
2017-08-10 17:34:37 +0100

    Function to connect to bucket

--- lib/s3sync/bucket.rb
+++ lib/s3sync/bucket.rb
@@ -12,5 +12,13 @@ module S3sync
         object.contents
       end.flatten
     end
+
+    def self.connect(url:,access_key_id:,secret_access_key:, region: "eu-west-1", bucket_name:)
+      client = Aws::S3::Client.new(
+        endpoint: url,
+        credentials: Aws::Credentials.new(access_key_id, secret_access_key),
+        region: region)
+      Bucket.new(client: client, name: bucket_name)
+    end
   end
 end
